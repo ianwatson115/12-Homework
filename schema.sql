77c304350bbad7a7d8d62071707cec15b46d86b2
ian <ianwatson115@gmail.com>
2020-02-20 14:00:05 -0500

    created database tables

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bigcompany_db;
+
+CREATE DATABASE bigcompany_db;
+
+USE bigcompany_db;
+
+
+CREATE TABLE department (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id int NOT NULL AUTO_INCREMENT,
+  title varchar(30) NOT NULL,
+  salary decimal(10,2),
+  department_id int NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int NOT NULL,
+  manager_id int,
+  PRIMARY KEY (id)
+);
